3e4d82888b13a69400e9c6baa9c9b81126004517
guwls <guswls2654@naver.com>
2024-10-16 01:33:15 +0900

    API 명세, ERD, 시퀀스 다이어그램 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.spring)
-    alias(libs.plugins.spring.boot)
+    id("org.springframework.boot") version "3.0.0"
     alias(libs.plugins.spring.dependency.management)
     id("jacoco")
 }
@@ -23,6 +23,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation(libs.spring.boot.starter.web)
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
     annotationProcessor(libs.spring.boot.configuration.processor)
     testImplementation(libs.spring.boot.starter.test)
 }
@@ -38,18 +42,21 @@ with(extensions.getByType(JacocoPluginExtension::class.java)) {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        // support JSR 305 annotation ( spring null-safety )
+        // support JSR 305 annotation (spring null-safety)
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
+
 // bundling tasks
 tasks.getByName("bootJar") {
     enabled = true
 }
+
 tasks.getByName("jar") {
     enabled = false
 }
+
 // test tasks
 tasks.test {
     ignoreFailures = true
